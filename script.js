7772430289e08e6d5bc1d118c6f8af5aa908a81f
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-03-01 01:41:46 +0600

    add js for changing random color by clicking btn

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-console.log("hi from js.....")
 
 // set alert for complete btn
 
 function alertBtn(){
     alert("Board updated Successfully");
 }
+
+// function for changing theme color of the website
+
+const changeThemeColorBtn = document.getElementById("changeThemeColorBtn");
+let randomNumber = () => {
+    return Math.floor(Math.random() * 256); 
+}
+
+let changeColor = () => {
+    let color = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+    document.body.style.backgroundColor = color;
+}
+changeThemeColorBtn.addEventListener("click", changeColor);
     
 
