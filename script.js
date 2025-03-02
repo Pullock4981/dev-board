bf7e985f38002dead3364b1037a82c3157be7472
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-03-02 15:40:23 +0600

    update js and all of the code

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
-
-// set alert for complete btn
-
-function alertBtn(){
-    alert("Board updated Successfully");
+// convertion functions
+function innerTextToNumberById (Id) {
+    newValue = parseInt(document.getElementById(Id).innerText);
+    return newValue;
+}
+function valueToNumberById (Id) {
+    return parseInt(document.getElementById(Id).value);
 }
 
-// function for changing theme color of the website
+// activity functions
+let taskTitle = document.getElementsByClassName("task-title")
+const divHistory = document.getElementById("activity-container");
+function activity (indexOfCard) {
+    
+    const divUpdate = document.createElement("div");
+       
+    divUpdate.innerHTML = `
+            <hr class="border-dashed border-1 border-gray-300 mb-5">
+            <div class="card bg-[#F4F7FF] px-8 py-5 m-3">
+                <p>
+                    You have complited the task <span class="font-bold">${
+                        taskTitle[indexOfCard].innerText}
+                    </span> at <span class="font-bold">${timeNow}</span> 
+                </p>
+            </div>
+        `
+    divHistory.appendChild(divUpdate)
+    return
+}
+
+
+// js for alert
+
+// task done
+let taskDone = 0;
+
+// task left
+let taskLeft = document.getElementsByClassName("task").length;
+document.getElementById("task-left").innerText = taskLeft;
+
+// task title
+
+console.log(taskTitle)
+
+// button alert
+for (let i = 0; i < document.getElementsByClassName("btn-alert").length; i++) {
+    document.getElementsByClassName("btn-alert")[i].addEventListener("click", function () {
+        alert("Board Updated Successfully");
+        // task done
+        taskDone = innerTextToNumberById("task-done") + 1;
+        document.getElementById("task-done").innerText = taskDone;
+        // task left
+        taskLeft = taskLeft - 1;
+        console.log(taskLeft);
+        document.getElementById("task-left").innerText = taskLeft;
+        this.disabled = true;
+        // task history
+        activity (i)
+        // all tasks completed
+        if (taskLeft === 0) {
+            alert("You have completed all the tasks");
+        }
+    });
+}
+
+// js for clean history
+
+document.getElementById("clear-history").addEventListener("click", function () {
+    document.getElementById("activity-container").innerHTML = "";
+}
+)
 
 const changeThemeColorBtn = document.getElementById("changeThemeColorBtn");
 let randomNumber = () => {
@@ -17,27 +80,19 @@ let changeColor = () => {
     document.body.style.backgroundColor = color;
 }
 changeThemeColorBtn.addEventListener("click", changeColor);
-    
 
-// function for update date and time
+
+// changing current time
 
 let today = new Intl.DateTimeFormat("en-bd", {
     dateStyle: "full"
 }).format(new Date());
  
-document.getElementById("date").innerText = today;
+document.getElementById("time-now").innerText = today;
 
 let timeNow = new Intl.DateTimeFormat("en-bd", {
     timeStyle: "medium"
-    
 }).format(new Date());
 
-// function for activity section
 
 
-// function for clear history
-
-document.getElementById("clearHistory").addEventListener("click", function () {
-    document.getElementById("updated-task").innerHTML = "";
-}
-)
